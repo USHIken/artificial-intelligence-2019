2e0650dcd1f4eb17fb7ae227e51bdc8d5cf4e627
ushiken <u5h1k3n@gmail.com>
2019-11-28 16:49:35 +0900

    RBFによる非線形回帰を実装

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 
+axes = plt.subplot(1, 1, 1)
 
 def make_linear_data(n, a=1, b=0, noise=True):
     func = lambda x: a*x + b
@@ -19,7 +20,7 @@ def make_data(n, func, start, end, noise):
     x = np.linspace(start, end, n)
     data = pd.Series(func(x), index=x)
     if noise:
-        noise_func = lambda data, n: data + np.random.normal(0.0, 0.3, n)
+        noise_func = lambda data, n: data + np.random.normal(0.0, 0.1, n)
         data = noise_func(data, n)
     return data
 
@@ -42,34 +43,78 @@ def linear_regression(data):
     return data
 
 
-def nonlenear_regression(data, param):
+def polynomial_basis_func(data, param):
+
+    param += 1
+    N = data.shape[0]
+    x = data.index
+    y = data.values
+
+    phi_x = np.zeros((N, param))
+    Y = np.zeros(N)
+
+    for j in range(param):
+        phi_x[:,j] = pow(x, j)
+    phi_x_t = phi_x.T
+    W = np.dot(np.dot(np.linalg.inv(np.dot(phi_x_t, phi_x)), phi_x_t), y)
+    for j in range(param):
+        Y += W[j] * phi_x[:, j]
+    
+    return Y
+    
+
+def radial_basis_func(data, param, nu=2):
 
     N = data.shape[0]
     x = data.index
     y = data.values
-    phi_x = np.zeros((N, param+1))
-    Y = np.zeros(N)
 
-    for j in range(param+1):
-        phi_x[:,j] = pow(x, j)
+    A = np.array_split(x, param)
+
+    c = [0]*param
+    for j in range(param):
+        c[j] = sum(A[j]) / len(A[j])
+
+    s2 = [0]*param
+    for j in range(param):
+        s2[j] = np.linalg.norm(A[j] - c[j], ord=2)**2 / len(A[j])
+
+    phi_x = np.ones((N, param+1))
+    Y = np.zeros(N)
+    for j in range(param):
+        phi_x[:,j+1] = np.exp(-((x-c[j])**2) / (nu * s2[j]))
     phi_x_t = phi_x.T
     W = np.dot(np.dot(np.linalg.inv(np.dot(phi_x_t, phi_x)), phi_x_t), y)
-    for j in range(param+1):
-        Y += W[j] * pow(x, j)
+    for j in range(param):
+        Y += W[j] * phi_x[:, j]
 
-    data = pd.Series(Y, index=x)
+    # plotting RBF
+    for j in range(1,param):
+        df = pd.DataFrame(pd.Series(W[j]*phi_x[:, j], index=x))
+        df.plot(color='k', legend=None, ax=axes)
+    
+    return Y
+
+
+def nonlenear_regression(data, param):
+    #Y = polynomial_basis_func(data, param)
+    Y = radial_basis_func(data, param, nu=10)
+    data = pd.Series(Y, index=data.index)
     return data
 
 
 if __name__ == "__main__":
     N = 100
 
+    # lenear regression
     # noise_data = make_linear_data(N, b=3)
     # data = linear_regression(noise_data)
 
+    # nonlenear regression
     noise_data = make_sin_data(N)
-    data = nonlenear_regression(noise_data, 3)
+    data = nonlenear_regression(noise_data, 10)
 
-    df = pd.DataFrame({'fitted': data, 'noise': noise_data})
-    df.plot(color=('r', 'b'))
+    # plotting fitted and noisy data
+    df = pd.DataFrame({'fitted data': data, 'noisy data': noise_data})
+    df.plot.line(style=['r-', 'b.'], ax=axes)
     plt.show()
