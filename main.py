963a9096d901f1c14ebc8103f9ae44e8598c2835
ushiken <u5h1k3n@gmail.com>
2019-11-28 18:01:46 +0900

    refactor

--- main.py
+++ main.py
@@ -4,13 +4,13 @@ import matplotlib.pyplot as plt
 
 axes = plt.subplot(1, 1, 1)
 
-def make_linear_data(n, a=1, b=0, noise=True):
+def make_linear_data(n, a=1, b=0, noise={"loc":0.0, "scale":0.1}):
     func = lambda x: a*x + b
     data = make_data(n, func, -10, 10, noise)
     return data
 
 
-def make_sin_data(n, noise=True):
+def make_sin_data(n, noise={"loc":0.0, "scale":0.1}):
     func = np.sin
     data = make_data(n, func, -1*np.pi, 1*np.pi, noise)
     return data
@@ -20,7 +20,7 @@ def make_data(n, func, start, end, noise):
     x = np.linspace(start, end, n)
     data = pd.Series(func(x), index=x)
     if noise:
-        noise_func = lambda data, n: data + np.random.normal(0.0, 0.1, n)
+        noise_func = lambda data, n: data + np.random.normal(noise["loc"], noise["scale"], n)
         data = noise_func(data, n)
     return data
 
@@ -43,32 +43,20 @@ def linear_regression(data):
     return data
 
 
-def polynomial_basis_func(data, param):
+def polynomial_basis_func(x, param):
 
-    param += 1
-    N = data.shape[0]
-    x = data.index
-    y = data.values
+    N = x.shape[0]
 
     phi_x = np.zeros((N, param))
-    Y = np.zeros(N)
-
     for j in range(param):
         phi_x[:,j] = pow(x, j)
-    phi_x_t = phi_x.T
-    W = np.dot(np.dot(np.linalg.inv(np.dot(phi_x_t, phi_x)), phi_x_t), y)
-    for j in range(param):
-        Y += W[j] * phi_x[:, j]
     
-    return Y
+    return phi_x
     
 
-def radial_basis_func(data, param, nu=2):
+def radial_basis_func(x, param, nu=10):
 
-    N = data.shape[0]
-    x = data.index
-    y = data.values
-
+    N = x.shape[0]
     A = np.array_split(x, param)
 
     c = [0]*param
@@ -80,41 +68,54 @@ def radial_basis_func(data, param, nu=2):
         s2[j] = np.linalg.norm(A[j] - c[j], ord=2)**2 / len(A[j])
 
     phi_x = np.ones((N, param+1))
-    Y = np.zeros(N)
     for j in range(param):
         phi_x[:,j+1] = np.exp(-((x-c[j])**2) / (nu * s2[j]))
+    
+    return phi_x
+
+
+def nonlenear_regression(data, basis_func, param):
+
+    if basis_func == polynomial_basis_func:
+        param += 1  # j = 1 .. param
+
+    N = data.shape[0]
+    x = data.index
+    y = data.values
+
+    phi_x = basis_func(x, param)
     phi_x_t = phi_x.T
     W = np.dot(np.dot(np.linalg.inv(np.dot(phi_x_t, phi_x)), phi_x_t), y)
+    Y = np.zeros(N)
     for j in range(param):
         Y += W[j] * phi_x[:, j]
 
-    # plotting RBF
-    for j in range(1,param):
-        df = pd.DataFrame(pd.Series(W[j]*phi_x[:, j], index=x))
-        df.plot(color='k', legend=None, ax=axes)
-    
-    return Y
+    # plotting curve
+    # for j in range(param):
+    #     df = pd.DataFrame(pd.Series(W[j]*phi_x[:, j], index=x))
+    #     df.plot(color='k', legend=None, ax=axes)
 
-
-def nonlenear_regression(data, param):
-    #Y = polynomial_basis_func(data, param)
-    Y = radial_basis_func(data, param, nu=10)
     data = pd.Series(Y, index=data.index)
+
     return data
 
 
 if __name__ == "__main__":
     N = 100
-
-    # lenear regression
-    # noise_data = make_linear_data(N, b=3)
-    # data = linear_regression(noise_data)
+    POLY_PARAM_NUM = 3
+    RBF_PARAM_NUM = 10
+    NOISE = {"loc":0.0, "scale":0.3}
 
     # nonlenear regression
-    noise_data = make_sin_data(N)
-    data = nonlenear_regression(noise_data, 10)
+    noise_data = make_sin_data(N, noise=NOISE)
+    fitted_by_poly = nonlenear_regression(noise_data, polynomial_basis_func, POLY_PARAM_NUM)
+    fitted_by_rbf = nonlenear_regression(noise_data, radial_basis_func, RBF_PARAM_NUM)
 
     # plotting fitted and noisy data
-    df = pd.DataFrame({'fitted data': data, 'noisy data': noise_data})
-    df.plot.line(style=['r-', 'b.'], ax=axes)
+    series_dict = {}
+    series_dict['fitted by poly (M={})'.format(POLY_PARAM_NUM)] = fitted_by_poly
+    series_dict['fitted by rbf (M={})'.format(RBF_PARAM_NUM)] = fitted_by_rbf
+    series_dict['noise (loc={}, scale={})'.format(NOISE["loc"], NOISE["scale"])] = noise_data
+    df = pd.DataFrame(series_dict)
+    df.plot.line(style=['r-x', 'b-.', 'go'], ax=axes, title="data")
     plt.show()
