e50bae808dc238fb516c39cfec44a4c33fd13eb4
ushiken <u5h1k3n@gmail.com>
2019-12-02 16:42:05 +0900

    mlp (wip)

--- main.py
+++ main.py
@@ -73,7 +73,7 @@ def radial_basis_func(x, param, nu=10):
     return phi_x
 
 
-def nonlenear_regression(data, basis_func, param, regularizer=0, plot=None):
+def nonlenear_regression(data, basis_func, param, regularizer_lambda=0, plot=None):
 
     if basis_func == polynomial_basis_func:
         param += 1  # j = 1 .. param
@@ -101,6 +101,70 @@ def nonlenear_regression(data, basis_func, param, regularizer=0, plot=None):
     return data
 
 
+class MultilayerPerceptron(object):
+
+    def __init__(self, n_hidden=30, epochs=100, minibatch_size=1):
+        self.n_hidden = n_hidden
+        self.epochs = epochs
+        self.minibatch_size = minibatch_size
+    
+    def _forward(self, X):
+        # 隠れ層の総入力
+        z_in = np.dot(X, self.w_1) + self.b_1
+        # 隠れ層の総出力
+        z_out = np.tanh(z_in)
+        # 出力層の総入力
+        y_in = np.dot(z_in, self.w_2) + self.b_2
+        # 出力層の総出力
+        y_out = np.tanh(y_in)
+        return z_in, z_out, y_in, y_out
+    
+    def _tanh_derivate(self, a):
+        return 1 - np.tanh(a)**2
+    
+    def fit(self, X_train, y_train, X_valid, y_valid):
+        n_output = y_train.shape[0]
+        n_features = 1 # X_train.shape[1]
+
+        # 入力層->隠れ層 への重み
+        self.b_1 = np.zeros(self.n_hidden)
+        self.w_1 = np.random.normal(loc=0.0, scale=0.1, size=(n_features, self.n_hidden))
+
+        # 隠れ層->出力層 への重み
+        self.b_2 = np.zeros(n_output)
+        self.w_2 = np.random.normal(loc=0.0, scale=0.1, size=(self.n_hidden, n_output))
+
+        # 学習
+        for i in range(self.epochs):
+            indices = np.arange(X_train.shape[0])
+
+            # ミニバッチの反復処理
+            end = indices.shape[0] - self.minibatch_size + 1
+            for start_idx in range(0, end, self.minibatch_size):
+                batch_idx = indices[start_idx:start_idx+self.minibatch_size]
+
+                # 順伝搬
+                z_in, z_out, y_in, y_out = self._forward(X_train[batch_idx])
+
+                # 逆伝搬
+                # sigma_z = y_out - y_train[batch_idx]
+                # tanh_derivative_z = self._tanh_derivate(z_out)
+                # sigma_y = 
+                delta_w_2 = (y_out - y_train[batch_idx]) * z_out[batch_idx]
+                
+
+def _forward(X):
+    # 隠れ層の総入力
+    z_in = np.dot(X, w_1) + b_1
+    # 隠れ層の総出力
+    z_out = np.tanh(z_in)
+    # 出力層の総入力
+    y_in = np.dot(z_in, w_2) + b_2
+    # 出力層の総出力
+    y_out = np.tanh(y_in)
+    return z_in, z_out, y_in, y_out
+
+
 def plot_data(data, name, style, axes):
     df = pd.DataFrame({name: data})
     df.plot.line(style=style, ax=axes)
@@ -124,8 +188,8 @@ if __name__ == "__main__":
 
     # setup some data
     NOISE = {"loc":0.0, "scale":0.1}
-    POLY_PARAM_NUM = 3
-    RBF_PARAM_NUM = 10
+    POLY_PARAM_NUM = 10
+    RBF_PARAM_NUM = 30
 
     plt.figure()
     poly_ax = plt.subplot(2, 2, 1)
@@ -136,7 +200,7 @@ if __name__ == "__main__":
 
     noise_data = make_sin_data(N, noise=NOISE)
     # regularizer_lambda = np.exp(-18)
-    regularizer_lambda = 0.001
+    regularizer_lambda = 0.1
 
     # polynomial without regularization
     fitted_by_poly = nonlenear_regression(noise_data, polynomial_basis_func, POLY_PARAM_NUM, 0, plot=poly_ax)
