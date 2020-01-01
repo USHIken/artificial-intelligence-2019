0c473a24acae274d1c7c0586abece112b5421cbf
ushiken <u5h1k3n@gmail.com>
2020-01-01 22:52:37 +0900

    split mlp class

--- mlp.py
+++ mlp.py
@@ -2,9 +2,82 @@ from utils import *
 import pandas as pd
 import matplotlib.pyplot as plt
 
+class MultilayerPerceptron(object):
+
+    def __init__(self, n_hidden=10, epochs=10, eta=1, shuffle=True, seed=None):
+        self.shuffle = shuffle
+        self.random = np.random.RandomState(seed)
+        self.n_hidden = n_hidden
+        self.epochs = epochs
+        self.eta = eta
+
+        # n_output = np.unique(y_train).shape[0]
+        n_output = 1
+        n_features = 1 # X_train.shape[1]
+
+        # 入力層->隠れ層 への重み
+        self.b_1 = np.zeros(self.n_hidden)
+        self.w_1 = np.random.normal(loc=0.0, scale=0.1, size=(n_features, self.n_hidden))
+
+        # 隠れ層->出力層 への重み
+        self.b_2 = np.zeros(n_output)
+        self.w_2 = np.random.normal(loc=0.0, scale=0.1, size=(self.n_hidden, n_output))
+
+    def _forward(self, X):
+        # 隠れ層の総入力
+        z_in = np.dot(X, self.w_1) + self.b_1
+        # 隠れ層の総出力
+        z_out = np.tanh(z_in)
+        # 出力層の総入力
+        y_in = np.dot(z_out, self.w_2) + self.b_2
+        # 出力層の総出力
+        y_out = y_in
+
+        return z_in, z_out, y_in, y_out
+    
+    def _onehot(self, y, n_output):
+        onehot = np.zeros((n_output, y.shape[0]))
+        for idx, val in enumerate(y.astype(int)):
+            onehot[val, idx] = 1.
+        return onehot.T
+
+    def _tanh_derivate(self, a):
+
+        return 1 - np.tanh(a)**2
+    
+    def fit(self, X_train, y_train):
+
+        # 学習
+        for _ in range(self.epochs):
+            indices = np.arange(X_train.shape[0])
+
+            if self.shuffle:
+                self.random.shuffle(indices)
+
+            for idx in indices:
+
+                # 順伝搬
+                z_in, z_out, y_in, y_out = self._forward(X_train[idx])
+                print("y_train, y_out = {}, {}".format(y_train[idx], y_out))
+
+                # 逆伝搬
+                sigma_out = y_out - y_train[idx]
+                delta_w_2 = np.dot(z_out.T, sigma_out)
+                self.w_2 += self.eta * delta_w_2
+                delta_w_1 = np.dot(sigma_out, self.w_2.T) * np.dot(self._tanh_derivate(z_in), X_train[idx])
+                self.w_1 += self.eta * delta_w_1
+            print("w_1", self.w_1)
+            print("w_2", self.w_2)
+        return self
+    
+    def predict(self, X):
+        _, _, _, y_out = self._forward(X)
+        y_pred = np.argmax(y_out, axis=1)
+
+        return y_pred
 
 def util_mlp():
-    N = 10
+    N = 1000
     train = make_sin_data(N)
     test = make_sin_data(N)
     mlp = MultilayerPerceptron(epochs=1)
